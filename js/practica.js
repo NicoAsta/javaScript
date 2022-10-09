f1c9db88633a4a8000481b4a7cc157bbcf42a3a8
NicoAsta <nicoasta123@gmail.com>
2022-10-09 14:59:20 -0300

    segunda pre entrega

---
+++ js/practica.js
@@ -0,0 +1,57 @@
+/* Ejercicios de funciones Dalto:
+
+1) Compra una maquina que deja pasar solamente a los mayores de edad, la primer persona que pasa despues de las 2 am, no paga.
+
+2) Crear un mini sistema para registrar presentes (p) y ausentes (a) 
+ - Pasados los 30 dias, mostrar su situacion final del alumno
+ -Maximo 10% de ausencias, si se tiene mas esta reprobado.
+
+
+3) 
+
+
+Hecho por Dalto:
+
+
+1)
+
+ let free = false
+
+const validarCliente = (time) => {
+        let edad = prompt ("cual es tu edad");
+        if (edad > 18) {
+            if (time >= 2 && time < 7 && !free) {
+                alert ("podes pasar gratis");
+                free = true;
+            } else {
+                alert ("podes pasar pero tenes que pagar, son las x horas")
+            }
+        } else {
+            alert ("no vas a pasar")
+        }
+}
+
+
+2)
+
+*/
+
+let cantidad ;
+let alumnosTotales = [];
+let tomarAsistencia ; 
+
+cantidad = productos (`cuantos alumnos son?`)
+
+
+for (i=0 ; i < cantidad ; i++) {
+    alumnosTotales[i] = [prompt(`nombre del alumno `+ (i+ 1)),0];
+
+}
+
+tomarAsistencia = (nombre, estado) => {
+    let presencia = prompt(nombre);
+    if (presencia == p || presencia == P) {
+        alumnosTotales[i][1]++;
+    }
+}
+ 
--- js/prentregaNicolasAsta.js
+++ js/prentregaNicolasAsta.js
@@ -1,80 +1,181 @@
-let pregunta;
-let aprobacion = Boolean;
-let chocoTorta = 100;
-let cheesecake = 150;
-let tortaOreo = 200;
-let lemonPie = 150;
-let brownie = 100;
-let total = 0;
-let opc2 ;
+// Primer Pre entrega
 
-function mensajeAgregar() {
-    if (tortaAgregada == null || tortaAgregada == undefined) {
-        alert ("No se agrego ningun producto")
-    }
+// let pregunta;
+// let aprobacion = Boolean;
+// let chocoTorta = 100;
+// let cheesecake = 150;
+// let tortaOreo = 200;
+// let lemonPie = 150;
+// let brownie = 100;
+// let total = 0;
+// let opc2 ;
 
-   else { alert (`Se agrego un/a ${tortaAgregada} a tu pedido, el total es de $ ${total}.-`)
-   }
-}
+// function mensajeAgregar() {
+//     if (tortaAgregada == null || tortaAgregada == undefined) {
+//         alert ("No se agrego ningun producto")
+//     }
 
-aprobacion = confirm("Queres comprar una torta?");
+//    else { alert (`Se agrego un/a ${tortaAgregada} a tu pedido, el total es de $ ${total}.-`)
+//    }
+// }
 
-
-while (aprobacion != false) {
-
-
-    pregunta = prompt("Que torta queres comprar? (escriba un numero) \n 1) Chocotorta \n 2) Cheesecake \n 3) Torta Oreo \n 4) Lemon Pie \n 5) Brownie")
+// aprobacion = confirm("Queres comprar una torta?");
 
 
-    switch (pregunta) {
-        case "1":
-            total += chocoTorta
-            tortaAgregada = "Chocotorta"
+// while (aprobacion != false) {
+
+
+//     pregunta = prompt("Que torta queres comprar? (escriba un numero) \n 1) Chocotorta \n 2) Cheesecake \n 3) Torta Oreo \n 4) Lemon Pie \n 5) Brownie")
+
+
+    // switch (pregunta) {
+    //     case "1":
+    //         total += chocoTorta
+    //         tortaAgregada = "Chocotorta"
             
-            break;
+    //         break;
 
-        case "2":
-            total += cheesecake
-            tortaAgregada = "Cheesecake"
+    //     case "2":
+    //         total += cheesecake
+    //         tortaAgregada = "Cheesecake"
             
-            break;
+    //         break;
 
-        case "3":
-                total += tortaOreo
-                tortaAgregada = "torta Oreo"
+    //     case "3":
+    //             total += tortaOreo
+    //             tortaAgregada = "torta Oreo"
                
-                break;
+    //             break;
 
-        case "4": 
-                total += lemonPie            
-                tortaAgregada = "Lemon Pie"
-                break;
+    //     case "4": 
+    //             total += lemonPie            
+    //             tortaAgregada = "Lemon Pie"
+    //             break;
 
-        case "5": 
-                total += brownie            
-                tortaAgregada = "Brownie"
-                break; 
+    //     case "5": 
+    //             total += brownie            
+    //             tortaAgregada = "Brownie"
+    //             break; 
 
-        case null:
-            tortaAgregada = null;
-            break;
+    //     case null:
+    //         tortaAgregada = null;
+    //         break;
 
-        default: alert("no hacemos esa torta")
-        tortaAgregada = undefined;
-            break;
-    }
+    //     default: alert("no hacemos esa torta")
+    //     tortaAgregada = undefined;
+    //         break;
+    // }
 
-    mensajeAgregar ();
+    // mensajeAgregar ();
 
-    if (pregunta != null ){
-    aprobacion = confirm("Queres algo mas?");
-    }
-    else {aprobacion = false}
+    // if (pregunta != null ){
+    // aprobacion = confirm("Queres algo mas?");
+    // }
+    // else {aprobacion = false}
     
        
-}
+// }
 
-if (total > 0) {
-   opc2 = alert (`tu total a pagar es de $ ${total} \n Gracias vuelvas prontos`)
-}
-else {alert("Usted cancelo la compra")}
+// if (total > 0) {
+//    opc2 = alert (`tu total a pagar es de $ ${total} \n Gracias vuelvas prontos`)
+// }
+// else {alert("Usted cancelo la compra")}
+
+
+//Segunda Pre Entrega - Nicolas Asta.
+
+let aprobacion = Boolean;
+
+
+
+const productos = [
+    {id: 1 , nombre: `Chocotorta`, precio: 100, tamanio: `Grande`},
+    {id: 2 , nombre: `Cheesecake`, precio: 150, tamanio: `Grande`},
+    {id: 3 , nombre: `Torta Oreo`, precio: 200, tamanio: `Grande`},
+    {id: 4 , nombre: `Lemon Pie`, precio: 150, tamanio: `Grande`},
+    {id: 5 , nombre: `Brownie`, precio: 100, tamanio: `Grande`},
+];
+
+let carrito = [];
+
+function Producto (id, nombre, precio, tamaño) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+    this.tamaño = tamaño;
+
+};
+
+aprobacion = confirm(`Queres comprar una torta?`);
+
+    if (aprobacion) {
+        alert(`Genial! A continuacion le mostramos nuestro catalogo de tortas en stock`)
+
+        let listaProductos = productos.map ((producto) => { 
+            return producto.nombre + ` ` + `Valor: ` +  producto.precio + `$` + ` tamaño: ` + producto.tamanio})
+        
+               alert(listaProductos.join (` \n - `))
+
+    
+
+        while (aprobacion) {
+      let producto =  prompt (`Agrega un producto a tu carrito. Por favor escribe el nombre de la torta para continuar.`)
+      let precio = 0
+
+       if  (producto == `Chocotorta` || producto == `Cheesecake` || producto == `Torta Oreo` || producto == `Lemon Pie`|| producto == `Brownie`) {
+        switch (producto) {
+            case "Chocotorta":
+                precio = 100
+                
+                break;
+    
+            case "Cheesecake":
+                precio = 150
+                
+                break;
+    
+            case "Torta Oreo":
+                    precio = 200
+                   
+                    break;
+    
+            case "Lemon Pie": 
+                   precio = 150
+                    break;
+    
+            case "Brownie": 
+                    precio = 100
+                    break; 
+    
+            case null:
+                tortaAgregada = null;
+                break;
+    
+            default: alert("no hacemos esa torta")
+            tortaAgregada = undefined;
+                break;
+        }
+    
+            let unidades = parseInt(prompt (`Cuantas unidades desea comprar?`));
+
+            carrito.push ({producto, unidades, precio})
+        
+         }
+       
+       }
+
+        aprobacion = prompt (`Desea seguir comprando?`)
+            if (aprobacion) {
+                carrito.forEach ((carritoFinal) => {
+                    alert (`Gracias, usted seleccionó ${carritoFinal.unidades} ${carritoFinal.producto}, con un valor total de $ ${carritoFinal.precio * carritoFinal.unidades} `)
+                })
+                
+            }   else {aprobacion == false}; 
+
+    }
+
+    
+        
+
+
+
